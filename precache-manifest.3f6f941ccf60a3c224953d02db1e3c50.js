65e41aab07e69b6fd191e986a1182aacf177a7a7
Andres Arevalo <aarevalo@smartgridcis.com>
2020-09-30 14:15:24 -0500

    Updates

---
+++ precache-manifest.3f6f941ccf60a3c224953d02db1e3c50.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ebe048d77e10fd52034a48320dc155f",
+    "url": "/news-arevalo-andresleo/index.html"
+  },
+  {
+    "revision": "323e083764d9c5d26632",
+    "url": "/news-arevalo-andresleo/static/css/2.97ca6098.chunk.css"
+  },
+  {
+    "revision": "8a5d0eaccc39a442b0af",
+    "url": "/news-arevalo-andresleo/static/css/main.a9fd75d4.chunk.css"
+  },
+  {
+    "revision": "323e083764d9c5d26632",
+    "url": "/news-arevalo-andresleo/static/js/2.9b29ff83.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/news-arevalo-andresleo/static/js/2.9b29ff83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5d0eaccc39a442b0af",
+    "url": "/news-arevalo-andresleo/static/js/main.5edd119b.chunk.js"
+  },
+  {
+    "revision": "fc81381c7ed4a7fc29af",
+    "url": "/news-arevalo-andresleo/static/js/runtime-main.cf583360.js"
+  },
+  {
+    "revision": "69accbef687f498fc0c1c5cf508c38a4",
+    "url": "/news-arevalo-andresleo/static/media/avion.69accbef.png"
+  }
+]);
